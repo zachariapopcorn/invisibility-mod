99eebfbe015b0b35d408222650e063d8a14ed001
zachariapopcorn <robloxandminecraft19976@gmail.com>
2021-06-24 13:13:09 -0700

    Cleanup and update

---
+++ classes/Utils.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Il2CppSystem.Collections.Generic;
+
+class Utils
+{
+
+    public static void SetColor(SpriteRenderer renderer, float alpha)
+    {
+        renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, alpha);
+    }
+
+    public static PlayerControl GetPlayerById(byte id)
+    {
+        List<PlayerControl> players = PlayerControl.AllPlayerControls;
+        for (int i = 0; i < players.Count; i++)
+        {
+            PlayerControl player = players[i];
+            if (player.Data.PlayerId == id)
+            {
+                return player;
+            }
+        }
+        return null;
+    }
+}
